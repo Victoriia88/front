b5bf923f134461492557874a8be532ce55e81ede
Victoriia88 <vulynets528@gmail.com>
2023-04-26 20:32:50 +0300

    create homework 4

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-firstName = prompt('What\'s your name');
-alert('Hello, ' + firstName + ' ! How are you?');
+// firstName = prompt('What\'s your name');
+// alert('Hello, ' + firstName + ' ! How are you?');
+
+hours = prompt('Specify the number of hours?');
+hoursToNumber = parseInt (hours);
+seconds = hoursToNumber * 60;
+alert('In ' + hoursToNumber + ' hours we have ' + seconds + ' seconds!');
