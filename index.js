a56ddde461e2fb230fe5c0aba141f138953c7c86
Victoriia88 <vulynets528@gmail.com>
2023-05-17 18:30:09 +0300

    create homework 11

--- index.js
+++ index.js
@@ -17,20 +17,41 @@
 // alert (firstNumber + '*' + secondNumber + '=' + multiplication);
 // alert (firstNumber + '/' + secondNumber + '=' + divide);
 
-let numOrStr = prompt('input number or string');
-console.log(numOrStr);
+// let numOrStr = prompt('input number or string');
+// console.log(numOrStr);
 
-switch (true) {
-  case numOrStr === null:
-    console.log('ви скасували');
-    break;
-  case numOrStr.trim() === '':
-    console.log('Empty String');
-    break;
-  case isNaN(+numOrStr):
-    console.log('number is Ba_NaN');
-    break;
-  default:
-    console.log('OK!');
-    break;
+// switch (true) {
+//   case numOrStr === null:
+//     console.log('ви скасували');
+//     break;
+//   case numOrStr.trim() === '':
+//     console.log('Empty String');
+//     break;
+//   case isNaN(+numOrStr):
+//     console.log('number is Ba_NaN');
+//     break;
+//   default:
+//     console.log('OK!');
+//     break;
+// }
+
+
+var array = [];
+var length = parseInt(prompt("Enter the length of the array:"));
+
+for (var i = 1; i <= length; i++) {
+  var element = parseInt(prompt("Enter the array element at position " + i + ":"));
+  array.push(element);
 }
+
+array.sort(function(a, b) {
+  return a - b;
+});
+
+console.log("Array before removing elements: " + array);
+
+array.splice(1, 4);
+
+console.log("Array after removing elements: " + array);
+
+
