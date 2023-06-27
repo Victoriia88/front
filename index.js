25be09659bc8a44d558cbcc3209878cadcca9dac
Victoriia88 <vulynets528@gmail.com>
2023-06-27 19:01:58 +0300

    create HomeWork5

--- index.js
+++ index.js
@@ -1,57 +1,118 @@
-// firstName = prompt('What\'s your name');
-// alert('Hello, ' + firstName + ' ! How are you?');
+// 1
+let taskFirst = 10;
 
-// hours = prompt('Specify the number of hours?');
-// hoursToNumber = parseInt (hours);
-// seconds = hoursToNumber * 60 * 60;
-// alert('In ' + hoursToNumber + ' hours we have ' + seconds + ' seconds!');
+while (taskFirst <= 20) {
+  document.write(taskFirst);
+  
+  if (taskFirst < 20) {
+    document.write(", ");
+  }
+  
+  taskFirst++;
+}
+// 2 
+let taskSecond = 10;
 
-// firstNumber = prompt ('Please enter the first number?');
-// secondNumber = prompt ('Please enter the second number?');
-// minus = (+firstNumber) - (+secondNumber);
-// plus = (+firstNumber) + (+secondNumber);
-// multiplication = (+firstNumber) * (+secondNumber);
-// divide = (+firstNumber) / (+secondNumber);
-// alert (firstNumber + '-' + secondNumber + '=' + minus);
-// alert (firstNumber + '+' + secondNumber + '=' + plus);
-// alert (firstNumber + '*' + secondNumber + '=' + multiplication);
-// alert (firstNumber + '/' + secondNumber + '=' + divide);
+while (taskSecond <= 20){
+taskSecond++;
 
-// let numOrStr = prompt('input number or string');
-// console.log(numOrStr);
-
-// switch (true) {
-//   case numOrStr === null:
-//     console.log('ви скасували');
-//     break;
-//   case numOrStr.trim() === '':
-//     console.log('Empty String');
-//     break;
-//   case isNaN(+numOrStr):
-//     console.log('number is Ba_NaN');
-//     break;
-//   default:
-//     console.log('OK!');
-//     break;
-// }
-
-
-var array = [];
-var length = parseInt(prompt("Enter the length of the array:"));
-
-for (var i = 1; i <= length; i++) {
-  var element = parseInt(prompt("Enter the array element at position " + i + ":"));
-  array.push(element);
+console.log(taskSecond * taskSecond);
 }
 
-array.sort(function(a, b) {
-  return a - b;
-});
 
-console.log("Array before removing elements: " + array);
+// 3
+let taskThird = 1;
 
-array.splice(1, 4);
+while (taskThird <= 10){
+taskThird++;   
 
-console.log("Array after removing elements: " + array);
+console.log(7 * taskThird);
+}
 
+// 4
+let sum = 0;
+let taskFour = 1;
 
+while (taskFour <= 15){
+sum += taskFour;
+taskFour++;
+}
+
+console.log(sum);
+
+// 5
+let taskFive = 15;
+let multiplication = 1;
+
+while (taskFive <= 35){
+multiplication *= taskFive;
+taskFive++;
+}
+
+console.log(multiplication);
+
+// 6
+let taskSix = 1;
+let sumSix = 0;
+let average = 0;
+
+while (taskSix <= 500){
+sumSix += taskSix;
+taskSix++;
+average++;
+}
+
+console.log(sumSix/average);
+
+// 7
+let taskSeven = 30;
+let sumSeven = 0;
+
+while (taskSeven <= 80){
+    if (!(taskSeven%2)){
+        sumSeven +=  taskSeven;
+    }
+taskSeven++;
+}
+
+console.log(sumSeven);
+
+// 8
+let taskEight = 100;
+
+while (taskEight <= 200){
+    if (!(taskEight%3)){
+    console.log(taskEight);
+    }
+taskEight++;
+}
+
+// 9-10-11
+let taskNine = parseInt(prompt('Please enter any number?'));
+let i = 1;
+let countTen = 0;
+let sumEleven = 0;
+
+while (i <= taskNine){
+    if(taskNine % i === 0){
+    console.log(i);
+    if(i % 2 === 0){
+        countTen++;
+        sumEleven += i;
+    }
+}
+    i++;
+}
+
+console.log(`Count even numbers = ${countTen}. Sum of even numbers= ${sumEleven} `);
+
+// 12
+let firstNum = 1;
+while (firstNum <= 10) {
+  let secondNum = 1;
+  while (secondNum <= 10) {
+    console.log(`${firstNum} * ${secondNum} = ${firstNum*secondNum}`);
+    secondNum++;
+  }
+  firstNum++;
+}
